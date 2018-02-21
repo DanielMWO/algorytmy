6170e69f69f21f87e63374eb2c2212da3f0d65a3
Daniel Polek-Pawlak <polekpaw@student.agh.edu.pl>
2018-02-21 22:54:57 +0100

    zadanie 2 done

--- zadania/zad2.py
+++ zadania/zad2.py
@@ -1,35 +1,31 @@
-# coding=utf-8 
-'''
-Created on 21 lut 2018
-
-@author: Daniel
-'''
-
-import sys
-sys.setrecursionlimit(10000000)
-
-if __name__ == '__main__':
-    #defninujemy funkcje do zamiany miejscami  cyfr w lciznie
-    #def reverseOrder (int):
-        
-    print (9//10)
-        
-    # Denfiniujemy funkcje do sprawdzania czy czy coś jest palindormem
-    def isPalindrom (n):
-        assert type(n) == int
-        if (1):
-            a = str(n)
-            b = a[::-1]
-            n2 = int(b)
-            if a == b:
-                print (" the palindrom is " + b) 
-            else:
-                isPalindrom(n+n2)
-    isPalindrom(200) 
-    
-    
-     
-    for i in range(1,200):
-        print ("for" + str(i))
-        isPalindrom(i)
-        
+# coding=utf-8   
+''''' 
+Created on 21 lut 2018 
+ 
+@author: Daniel 
+'''  
+import sys  
+sys.setrecursionlimit(1000)  # To pozwala nam zwiększyć liczbę możliwych wywołań rekurencyjnych (do momentu aż skończy się pamięć)  
+  
+  
+if __name__ == '__main__':  
+   
+  
+    #Definiujemy funkcje do sprawdzania czy coś jest Palindromem,  Funkcja operuje na stringach (zamiast np na listach) bo python pozwala na takie cuda  
+    def isPalindrom (n):  
+        assert type(n) == int  
+        a = str(n)   # zmieniamy integer na string    
+        b = a[::-1]  # odwracamy string   
+        n2 = int(b)  # zamieniamy odwrócony string na integer   
+        if a == b:     
+            return b  # zwracamy string jeżeli jest Palindromem  
+        else:  
+            return isPalindrom(n+n2) # jeśli nie jest powtarzamy operację dla sumy  dwóch liczb jako parametru      
+       
+    for i in range(1,201):  # wykonujemy funkcję w pętli dla zadanego przedziału i drukujemy wyniki   
+        try:  
+            print ("for {0} the palindorm is {1}".format(i, isPalindrom(i)))  
+              
+        except (RecursionError):  
+            print ("Sorry not enough memory to calculate palindrom for {0} - skipping ".format(i))  ## obsługujemy wyjątek w przypadku gdy przekroczymy dopuszczalną przez system liczbę rekurencji  
+            continue  
